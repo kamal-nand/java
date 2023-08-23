428d3dc525fa84a119d168955c45ef3d5a45261a
kamal-nand <136990821+kamal-nand@users.noreply.github.com>
2023-08-23 20:23:01 +0530

    Find Average

---
+++ average.java
@@ -0,0 +1,11 @@
+public class average {
+    public static int avrg(int a, int b, int c) {
+        return (a+b+c)/3;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println( avrg(5, 2, 2));
+
+    }
+}
